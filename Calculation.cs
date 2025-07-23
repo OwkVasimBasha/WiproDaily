28e19e2728d1b110a92fa85f2ec8f11043303b8c
OwkVasimBasha <owkvasim@gmail.com>
2025-07-23 22:41:50 +0530

    commit -m "commit Day3"

--- Calculation.cs
+++ Calculation.cs
@@ -4,38 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Helloworld
+namespace CalculationLibrary
 {
-    internal class Calculation
+    public class Calculation
     {
-        public int sum(int a , int b)
+        public int Sum(int a, int b)
         {
             return a + b;
         }
-        public int sub(int a, int b)
+        public int Sub(int a, int b)
         {
             return a - b;
         }
-
-        public int mul(int a,int b)
+        public int Mult(int a, int b)
         {
             return a * b;
         }
-        static void Main()
-        {
-            int a, b;
-            Console.WriteLine("Enter the two numbers");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            Calculation c = new Calculation();
-            int result = c.sum(a, b);
-            Console.WriteLine("The sum of two numbers are"+result);
-            result = c.sub(a, b);
-            Console.WriteLine("The sub of two numbers are"+ result);
-            result = c.mul(a, b);
-            Console.WriteLine("The multiplication of two numbers are"+result);
-
-            
-        }
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentDemo;
 
-namespace Helloworld
+
+namespace StudentClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow World");
+            
+
+
         }
     }
 }
