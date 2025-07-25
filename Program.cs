d3487a9b89ad8904eb11a098e0fa66f4a3b27044
OwkVasimBasha <owkvasim@gmail.com>
2025-07-25 16:26:06 +0530

    git commit -m "Day3"

--- Program.cs
+++ Program.cs
@@ -5,14 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using StudentDemo;
 
-
-namespace StudentClient
+namespace StudentClinet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Student s = new Student();
+            s.id =10;
+            s.name = "Basha";
+            s.salary = 100;
+            Console.WriteLine(s.id);
+            Console.Write(s.name);
+            Console.WriteLine(s.salary);
+
 
 
         }
